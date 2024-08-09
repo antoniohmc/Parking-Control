fae217878f2b3db9e991e7264622394c426a5e28
Antonio Campos <antoniomallmann01@gmail.com>
2024-08-09 10:37:58 -0300

    iniciando criação de um sistema de estacionamento.

---
+++ src/main/java/com/api/parking_control/model/ParkingSpotModel.java
@@ -0,0 +1,14 @@
+package com.api.parking_control.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    private 
+
+}
