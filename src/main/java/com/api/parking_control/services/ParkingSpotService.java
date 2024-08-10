19849edb35dd4465dd3ff6b283836edbcaeecfac
Antonio Campos <antoniomallmann01@gmail.com>
2024-08-10 01:30:32 -0300

    implementando o método save, para armazenar um model no banco de dados.

--- src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parking_control.services;
 
+import com.api.parking_control.model.ParkingSpotModel;
 import com.api.parking_control.repository.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,10 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
